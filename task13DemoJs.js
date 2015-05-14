631c02fac885edcd48f4d70069180f03aff48d80
Ahmad Nazhir <anazhir@andrew.cmu.edu>
2015-05-13 22:06:37 -0400

    change ui button

--- task13DemoJs.js
+++ task13DemoJs.js
@@ -37,10 +37,20 @@ $( document ).on( "pagecreate", "#map-page", function() {
         var myOptions = {
             zoom: 12,
             center: latlng,
+            disableDefaultUI: true,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
         this.map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+
+        // Create the DIV to hold the control and
+        // call the CenterControl() constructor passing
+        // in this DIV.
+        var centerControlDiv = document.createElement('div');
+        var centerControl = new CenterControl(centerControlDiv, map, latlng);
+
+        centerControlDiv.index = 1;
+        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerControlDiv);
 
     		// Create a renderer for directions and bind it to the map.
     	  var rendererOptions = {
@@ -52,7 +62,7 @@ $( document ).on( "pagecreate", "#map-page", function() {
         directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
     	  // Instantiate an info window to hold step text.
-    	  stepDisplay = new google.maps.InfoWindow();
+    	  // stepDisplay = new google.maps.InfoWindow();
         // Retrieving User Travel Origins and Destination Input Box
         var start = (document.getElementById('start'));
         var end = (document.getElementById('end'));
@@ -273,7 +283,7 @@ function calcRoute() {
       this.interval = setInterval(function(){loadVehicle(rt); },10000);
 
       directionsDisplay.setDirections(response);
-      showSteps(response);
+      //showSteps(response);
     }
   });
 }
@@ -302,4 +312,37 @@ function attachInstructionText(marker, text) { // this for creating instructions
     stepDisplay.setContent(text);
     stepDisplay.open(map, marker);
   });
+}
+
+function CenterControl(controlDiv, map, position) {
+
+  // Set CSS for the control border
+  var controlUI = document.createElement('div');
+  controlUI.style.backgroundColor = '#eeeeee';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '3px';
+  controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+  controlUI.style.cursor = 'pointer';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.marginRight = '5px';
+  controlUI.style.textAlign = 'center';
+  controlUI.title = 'Click to recenter the map';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '16px';
+  controlText.style.lineHeight = '38px';
+  controlText.style.paddingLeft = '5px';
+  controlText.style.paddingRight = '5px';
+  controlText.innerHTML = 'My Location';
+  controlUI.appendChild(controlText);
+
+  // Setup the click event listeners: simply set the map to user location
+  google.maps.event.addDomListener(controlUI, 'click', function() {
+    map.setCenter(position)
+  });
+
 }
